636a0384aac1743f7821f6440795652e3de14f4e
Adam Duff <j.adam.duff@gmail.com>
2018-10-30 07:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function setPage() {
+  let pageTemplate = document.getElementById('page-template').innerHTML;
+  let templateFn = _.template(pageTemplate);
+  let pageContainer = document.querySelector('main');
+  let templateHTML = templateFn();
+  pageContainer.innerHTML += templateHTML;
+}
+
 function createPost() {
   let postTitle = document.getElementById('postTitle').value;
   let postBody = document.getElementById('postText').value;
@@ -7,5 +15,6 @@ function createPost() {
   let templateFn = _.template(postTemplate);
   let postContainer = document.getElementById('post');
   let templateHTML = templateFn({title: postTitle, body: postBody, author: postAuthor});
+  setPage();
   postContainer.innerHTML += templateHTML;
 }
