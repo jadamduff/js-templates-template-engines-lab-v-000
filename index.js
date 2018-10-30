e820490905376dc793f7f1219f88e7b1f5869b2d
Adam Duff <j.adam.duff@gmail.com>
2018-10-30 07:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ function setPage() {
   pageContainer.innerHTML += templateHTML;
 }
 
+function createPost() {
+  let postTitle = document.getElementById('postTtile').value;
+  let postBody = document.getElementById('postText').value;
+  let postAuthor = document.getElementById('postAuthor').value;
+
+  let postTemplate = document.getElementById('post-template').innerHTML;
+  let templateFn = _.template(postTemplate);
+  let postContainer = document.getElementById('post');
+  let templateHTML = templateFn({title: postTitle, body: postBody, author: postAuthor});
+  postContainer.innerHTML += templateHTML;
+}
+
 setPage();
