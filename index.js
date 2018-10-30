a7137debd27fea25b436818132b58a12d021a210
Adam Duff <j.adam.duff@gmail.com>
2018-10-30 07:21:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function setPage() {
+  let pageTemplate = document.getElementById('page-template').innerHTML;
+  let templateFn = _.template(pageTemplate);
+  let pageContainer = document.querySelector('main');
+  let templateHTML = templateFn();
+  pageContainer.innerHTML += templateHTML;
+}
